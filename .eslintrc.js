26e9e3c3e59aacfce8873bf612e123421a6a4935
Kevin <827778206@qq.com>
2019-05-02 22:26:59 +0800

    项目初始化

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es6': true
+	},
+	'extends': 'eslint:recommended',
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parserOptions': {
+		'ecmaVersion': 2018
+	},
+	'rules': {
+		'no-console': [
+			'error',
+			{ allow: ['warn', 'error', 'info'] }
+		],
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'warn',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
