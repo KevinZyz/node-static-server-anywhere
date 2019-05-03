4bf838d482e50d1e357b6276ac50c77a1ff9d157
Kevin <827778206@qq.com>
2019-05-03 12:40:21 +0800

    feat: 增加 MIME 文件类型的判断与处理

--- src/helper/route.js
+++ src/helper/route.js
@@ -5,6 +5,8 @@ const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
 const Handlebars = require('handlebars');
 
+const mime = require('./mime');
+
 
 const source = fs.readFileSync(path.join(__dirname, '../template/dir.html'));
 const template = Handlebars.compile(source.toString());
@@ -13,8 +15,9 @@ module.exports = async (req, res, filePath, config) => {
 	try{
 		const stats = await stat(filePath);
 		if(stats.isFile()){
+			const contentType = mime(filePath);
 			res.setStatusCode = 200;
-			res.setHeader('Content-Type', 'text/plain');
+			res.setHeader('Content-Type', contentType);
 			fs.createReadStream(filePath).pipe(res);
 		} else if(stats.isDirectory()){
 			const files = await readdir(filePath);
